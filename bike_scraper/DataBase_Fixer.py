0ac5a81919e668fb29c4661d3c4b3ad148d8128d
Devin Stacey <devin.stacey@ucdconnect.ie>
2016-04-04 13:59:12 +0100

    Database fix

--- bike_scraper/DataBase_Fixer.py
+++ bike_scraper/DataBase_Fixer.py
@@ -6,10 +6,10 @@ try:
     c = connection.cursor()
     c.execute('''ALTER TABLE Station_Details RENAME TO Station_Details_Temp ''')
     c.execute('''CREATE TABLE Station_Details(Station_Number INT PRIMARY KEY,
-                Station_Name CHAR(50), latitude REAL, longtitude REAL, Total_Spaces INT,
+                Station_Name CHAR(50), latitude REAL, longitude REAL, Total_Spaces INT,
                 Banking BOOLEAN, Bonus BOOLEAN)''')
     c.execute('''INSERT INTO Station_Details(Station_Number, Station_Name, latitude, longtitude, Total_Spaces, Banking, Bonus)
-            SELECT Station_Number, Station_Name, longtitude, latitude, Total_Spaces, Banking, Bonus FROM Station_Details_Temp''')
+            SELECT Station_Number, Station_Name, longitude, latitude, Total_Spaces, Banking, Bonus FROM Station_Details_Temp''')
     c.execute('''DROP TABLE Station_Details_Temp''')
     connection.commit()
     c.close()
