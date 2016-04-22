c4e5423a358707ce4bc4fb84aabcd12cf4611612
DevCarl <Devin.stacey@ucdconnect.ie>
2016-04-22 13:56:35 +0100

    Update DataBase_Fixer.py
    Updated code to follow Pep8 standards

--- bike_scraper/DataBase_Fixer.py
+++ bike_scraper/DataBase_Fixer.py
@@ -8,8 +8,10 @@ try:
     c.execute('''CREATE TABLE Station_Details(Station_Number INT PRIMARY KEY,
                 Station_Name CHAR(50), latitude REAL, longitude REAL, Total_Spaces INT,
                 Banking BOOLEAN, Bonus BOOLEAN)''')
-    c.execute('''INSERT INTO Station_Details(Station_Number, Station_Name, latitude, longitude, Total_Spaces, Banking, Bonus)
-            SELECT Station_Number, Station_Name, longtitude, latitude, Total_Spaces, Banking, Bonus FROM Station_Details_Temp''')
+    c.execute('''INSERT INTO Station_Details(Station_Number, Station_Name, 
+            latitude, longitude, Total_Spaces, Banking, Bonus)
+            SELECT Station_Number, Station_Name, longtitude, latitude, 
+            Total_Spaces, Banking, Bonus FROM Station_Details_Temp''')
     c.execute('''DROP TABLE Station_Details_Temp''')
     connection.commit()
     c.close()
